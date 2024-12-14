07c3e090a179b864d2443b2dba87acf8552874c2
Victor Mwachi <mudakizevictor@gmail.com>
2024-12-14 14:56:53 +0300

    installed weasy print

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,render_template
+import requests
+import os
+from weasyprint import HTML
 
+os.add_dll_directory(r"C:\Program Files\GTK3-Runtime Win64\bin")
+app = Flask(__name__)
+url="https://victormwachi.github.io/"
+url2="https://chatgpt.com/share/675d6915-0590-800b-916c-e64ceb1fb800"
 @app.route("/")
 def main():
-    return "hello victor long time"
+    res = requests.get(url)
+    # Convert HTML to PDF
+    HTML(string=res.text).write_pdf('output.pdf')
+    return res.text #render_template("test.html")
