d2c8238483ce4bc13cbf9836ffd0de877b90e470
victormwachi <mudakizevictor@gmail.com>
2024-12-15 15:49:22 +0300

    working app

--- app.py
+++ app.py
@@ -1,15 +1,55 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,send_file
 import requests
 import os
+import bs4
 from weasyprint import HTML
+from uuid import uuid4
+output_folder = "output_folder"
 
-os.add_dll_directory(r"C:\Program Files\GTK3-Runtime Win64\bin")
+
+
 app = Flask(__name__)
-url="https://victormwachi.github.io/"
-url2="https://chatgpt.com/share/675d6915-0590-800b-916c-e64ceb1fb800"
+
+
+
+class HtmlPdfConverter:
+    def __init__(self,url=None):
+        self.url=url
+
+    def set_url(url):
+        self.url=url
+
+    def convert(self):
+        #Get response from the url
+        res = requests.get(self.url)
+        #Get the page title
+        title=bs4.BeautifulSoup(res.text,'html.parser').title.text.strip()
+        #if the not title,default = "sts_pdf_converter"
+        if len(title)==0:
+            title = "sts_pdf_converter"
+
+        file_name = str(uuid4()) + title + ".pdf"
+        # create the output folder not exists
+        os.makedirs(output_folder, exist_ok=True)
+
+        # Construct the full file path
+        output_file_path = os.path.join(output_folder, file_name)
+
+        # Render HTML to PDF
+        HTML(string=res.text).write_pdf(output_file_path)
+
+        return output_file_path
+        
+
+url1="https://victormwachi.github.io/"   
 @app.route("/")
 def main():
-    res = requests.get(url)
+    
     # Convert HTML to PDF
-    HTML(string=res.text).write_pdf('output.pdf')
-    return res.text #render_template("test.html")
+    convert = HtmlPdfConverter("https://chatgpt.com/share/675ecaac-aad0-800b-828a-7ab7d341da93")
+    return convert.convert()
+    #return send_file(pdf_file, as_attachment=True, download_name='output.pdf')
+     #render_template("test.html")
+
+if __name__ =="__main__":
+    app.run(debug=True)
